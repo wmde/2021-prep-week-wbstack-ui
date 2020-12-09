b7efaab9403e04cd7b7fedf5a061e690c19fde55
Jakob Warkotsch <jakob.warkotsch@wikimedia.de>
2020-12-09 16:30:52 +0100

    Add headline and fluff

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,21 @@
 <template>
   <v-app>
     <v-main>
-      <v-btn elevation="2">Create a new Wikibase for evaluation!</v-btn>
-    </v-main>
+		<v-container>
+			<h1 class="text-h1">Wikibase Sandbox</h1>
+			<div class="text-subtitle-1">Create a Wikibase which you can view, edit, extend yourself. Feel free to experiment. Attention: The instance will be reset after 2 hours, so it is really only for experimenting!</div>
+		</v-container>
+		<v-container>
+			<create-sandbox-form />
+		</v-container>
+	</v-main>
   </v-app>
 </template>
 
 <script>
+import CreateSandboxForm from '@/components/CreateSandboxForm';
 export default {
   name: 'App',
+	components: { CreateSandboxForm },
 };
 </script>
