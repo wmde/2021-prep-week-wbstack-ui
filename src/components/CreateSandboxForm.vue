9710540448f24586efe28cdbe70546ee5bd38bcf
Jakob Warkotsch <jakob.warkotsch@wikimedia.de>
2020-12-09 17:56:32 +0100

    Show success pop-up on success

--- src/components/CreateSandboxForm.vue
+++ src/components/CreateSandboxForm.vue
@@ -4,6 +4,17 @@
 		<v-btn color="primary" elevation="2" x-large @click="createSandbox">
 			Create a new Wikibase sandbox!
 		</v-btn>
+
+		<v-dialog v-model="showSuccessDialog" max-width="500">
+			<v-card>
+				<v-card-title class="headline grey lighten-2">
+					Success!
+				</v-card-title>
+				<v-card-text>
+					Success! You can now sign in to your Wikibase sandbox: <a :href="signInLink">{{ signInLink }}</a>
+				</v-card-text>
+			</v-card>
+		</v-dialog>
 	</v-form>
 </template>
 
@@ -14,6 +25,7 @@ export default {
 	data() {
 		return {
 			signInLink: null,
+			showSuccessDialog: false,
 		}
 	},
 
@@ -24,7 +36,8 @@ export default {
 				body: JSON.stringify( {} ), // recaptcha goes here?
 			} ) ).json();
 
-			console.log( response.signInLink );
+			this.signInLink = response.signInLink;
+			this.showSuccessDialog = true;
 		},
 	}
 }
