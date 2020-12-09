417c2901b8616a74b6a818e934c1ccaf5227dc8d
Jakob Warkotsch <jakob.warkotsch@wikimedia.de>
2020-12-09 17:33:09 +0100

    Make API request on button click

--- src/components/CreateSandboxForm.vue
+++ src/components/CreateSandboxForm.vue
@@ -1,12 +1,31 @@
 <template>
 	<v-form>
 		<!-- recaptcha goes somewhere here -->
-		<v-btn color="primary" elevation="2" x-large>Create a new Wikibase sandbox!</v-btn>
+		<v-btn color="primary" elevation="2" x-large @click="createSandbox">
+			Create a new Wikibase sandbox!
+		</v-btn>
 	</v-form>
 </template>
 
 <script>
 export default {
 	name: 'CreateSandboxForm',
+
+	data() {
+		return {
+			signInLink: null,
+		}
+	},
+
+	methods: {
+		async createSandbox() {
+			const response = await ( await fetch( `${ process.env.VUE_APP_API_URL }/sandbox/create`, {
+				method: 'POST',
+				body: JSON.stringify( {} ), // recaptcha goes here?
+			} ) ).json();
+
+			console.log( response.signInLink );
+		},
+	}
 }
 </script>
